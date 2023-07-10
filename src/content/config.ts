a29cbe26988a58093fc78038d3c66fc793f61b06
Daria Gorchylina <daria.gorchylina@gmail.com>
2023-07-10 18:52:17 +0200

    remove blog collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,5 @@
 import { z, defineCollection } from "astro:content";
 
-const blogSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.string().optional(),
-    heroImage: z.string().optional(),
-    badge: z.string().optional(),
-});
-
 const storeSchema = z.object({
     title: z.string(),
     description: z.string(),
@@ -22,13 +13,10 @@ const storeSchema = z.object({
     heroImage: z.string().optional(),
 });
 
-export type BlogSchema = z.infer<typeof blogSchema>;
 export type StoreSchema = z.infer<typeof storeSchema>;
 
-const blogCollection = defineCollection({ schema: blogSchema });
 const storeCollection = defineCollection({ schema: storeSchema });
 
 export const collections = {
-    'blog': blogCollection,
     'store': storeCollection
 }
