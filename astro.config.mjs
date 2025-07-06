efecc546c4d8018ad435ea8c9096026c2613e04c
Daria Gorchylina <daria.gorchylina@gmail.com>
2025-07-06 17:08:05 +0100

    astra upgrade plus added projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://madelgeek.com',
   image: {
-    service: squooshImageService(),
-      cacheDir: "./.cache/image",
-      logLevel: 'error',
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    },
+    cacheDir: "./.cache/image",
+    logLevel: 'error',
   },
   integrations: [mdx(), sitemap(), tailwind()]
 });
